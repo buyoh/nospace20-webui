b8220848f84190e44b96ad1b03daa260d9c767c0
buyoh <15198247+buyoh@users.noreply.github.com>
2026-02-18 14:28:21 +0900

    Configure endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 
-export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  plugins: [
-    react(),
-    wasm(),
-  ],
-  build: {
-    // vite-plugin-wasm が生成する top-level await に必要
-    target: 'esnext',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    server: {
+      port: 5173,
+    },
+    plugins: [
+      react(),
+      wasm(),
+    ],
+    build: {
+      // vite-plugin-wasm が生成する top-level await に必要
+      target: 'esnext',
+    },
+  };
 });
