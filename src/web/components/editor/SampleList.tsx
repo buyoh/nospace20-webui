46c710ba1de2ae32c8d11a2e8f7cefd7ff8daf17
buyoh <15198247+buyoh@users.noreply.github.com>
2026-03-09 02:53:42 +0900

    Append urls

--- src/web/components/editor/SampleList.tsx
+++ src/web/components/editor/SampleList.tsx
@@ -15,21 +15,32 @@ interface SampleListProps {
  */
 export const SampleList: React.FC<SampleListProps> = ({ samples, onLoad }) => {
   return (
-    <div className="sample-list">
-      {samples.map((sample) => (
-        <div key={sample.name} className="sample-list__item">
-          <span className="sample-list__name">{sample.name}</span>
-          <button
-            className="sample-list__load-btn"
-            onClick={() => onLoad(sample.code)}
-          >
-            Load
-          </button>
-        </div>
-      ))}
-      {samples.length === 0 && (
-        <p className="sample-list__empty">サンプルはありません</p>
-      )}
+    <div>
+      <div className="sample-list">
+        {samples.map((sample) => (
+          <div key={sample.name} className="sample-list__item">
+            <span className="sample-list__name">{sample.name}</span>
+            <button
+              className="sample-list__load-btn"
+              onClick={() => onLoad(sample.code)}
+            >
+              Load
+            </button>
+          </div>
+        ))}
+        {samples.length === 0 && (
+          <p className="sample-list__empty">サンプルはありません</p>
+        )}
+      </div>
+      <div className="sample-list__footer">
+        <a href="https://github.com/buyoh/nospace20/blob/master/docs/spec.md">
+          Language Specification
+        </a>
+        {' '}/{' '}
+        <a href="https://github.com/buyoh/nospace20/blob/master/docs/tutorial.md">
+          Tutorial
+        </a>
+      </div>
     </div>
   );
 };
